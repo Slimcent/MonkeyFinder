8b6966ef163369bb3d768fd584a010eee9bc7aef
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-07-21 21:12:12 +0100

    MobileFinder app

---
+++ MonkeyFinder.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeyFinder.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MonkeyFinder.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MonkeyFinder.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
